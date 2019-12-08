e4096416396f56595766a2d1d1c2d7c09d1d4d6f
D00219161 <57397919+D00219161@users.noreply.github.com>
2019-12-08 23:59:42 +0000

    updated data and slider

--- react-app/src/components/Slider.jsx
+++ react-app/src/components/Slider.jsx
@@ -9,36 +9,39 @@ class Slider extends Component {
   <Carousel.Item>
     <img
       className="w-100"
-      src=""
-      alt="First Slide"
+      src="https://hb.imgix.net/75740eb26832ca9e583f573b09a294bcc47fb227.jpg?auto=compress,format&fit=crop&h=353&w=616&s=0a22e38765fcf80ab5d14cb123e6c3aa"
+      alt-text="Red Redemption 2"
     />
     <Carousel.Caption>
-      <h3>Apple Watch Series 4</h3>
-      <p></p>
+      <h3>Red Redemption 2</h3>
+      <p>Red Dead Redemption 2 is a 2018 action-adventure game developed and published by Rockstar Games. The game is the third entry in the Red Dead series and is a prequel to the 2010 game Red Dead Redemption.</p>
+      <p>€40.00</p>
     </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item>
     <img
       className="d-block w-100"
-      src=""
-      alt="Third slide"
+      src="https://a1.amlimg.com/MjA4MjQwZjBlNDUxMDA4MmU4ZDhmZDY2NjEzZWNjYjYmRfOKZwXfOGFXInnWnIu_aHR0cDovL21lZGlhLmFkc2ltZy5jb20vM2YxMmI2NzFlYzU1OWFiMzFmMDI2MWJmOTEzNjRmN2MxYTNmNWUwM2VlZDRiMTkyYzA0ZDE4YTliN2E4MTg4NS5qcGd8fHx8fHw3MDB4NTAwfGh0dHA6Ly93d3cuYWR2ZXJ0cy5pZS9zdGF0aWMvaS93YXRlcm1hcmsucG5nfHx8.jpg"
+      alt-text="Canon EOS 80D Body"
     />
 
     <Carousel.Caption>
-      <h3>Second slide label</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <h3>Canon EOS 80D Body</h3>
+      <p>At home or on your travels, at night or in daylight, explore the art of photography and filmmaking with a fast and reliable all-rounder camera that delivers stunning stills and Full HD movies.</p>
+      <p>€1,159.99</p>
     </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item>
     <img
       className="d-block w-100"
-      src=""
-      alt="Third slide"
+      src="https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTAaZrkgtqYfyxV6TDU3J8YtE0SuuSlPshz9Dx71IL2Xr15SC8cvA&usqp=CAc"
+      alt="Surface Pro X"
     />
 
     <Carousel.Caption>
-      <h3>Third slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+      <h3>Surface Pro X</h3>
+      <p>Edge to edge 2-in-1 laptop with LTE and 13” touchscreen. Ultra-thin and always connected, Surface Pro X combines ultimate mobility with amazing graphics.</p>
+      <p>€1,169.00</p>
     </Carousel.Caption>
   </Carousel.Item>
 </Carousel>
