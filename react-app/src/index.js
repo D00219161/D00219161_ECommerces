c4210179233590b39783e08c601486a4def62afa
D00219161 <57397919+D00219161@users.noreply.github.com>
2019-12-10 12:47:34 +0000

    sass

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -2,5 +2,5 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React    from 'react';
 import ReactDOM from 'react-dom';
 import App      from './components/App';
-
+import './lib/styles/main.css';
 ReactDOM.render(<App />, document.getElementById('root'));
