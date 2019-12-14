48af784b0166c9a605ecda4c942aa77fa8724999
D00219161 <57397919+D00219161@users.noreply.github.com>
2019-12-14 16:58:46 +0000

    updated database

--- react-app/src/components/Product.jsx
+++ react-app/src/components/Product.jsx
@@ -2,7 +2,8 @@ import React           from 'react';
 import AddToCartButton from './AddToCartButton';
 import * as Config     from '../config.json';
 import './Product.css';
-import {Col} from 'react-bootstrap/';
+import {Col, Row} from 'react-bootstrap/';
+import {VideoTrack } from 'video-react';
 
 class Product extends React.Component {
 
@@ -12,14 +13,15 @@ class Product extends React.Component {
   // components.
   render() {
     return (
-
-      <Col md={11}><div className='card mb-4'>
+      <Row>
+      <Col md={8}>
+      <div className='card mb-4'>
         <h2 className='card-header'>{this.props.product.name}</h2>
         <div className='row'>
 
           <div className='col-12 col-md-3'>
             <div className='product-image'>
-              <img src={this.props.product.image_href || Config.defaultImage} alt={this.props.product.name} />
+              <img src={this.props.product.image_href} alt={this.props.product.name} />
             </div>
           </div>
 
@@ -27,12 +29,25 @@ class Product extends React.Component {
             <div className='card-body'>
               <h3 className='card-sub-title'>&euro;{(parseFloat(this.props.product.price)).toFixed(2)}</h3>
               <p className='card-text'>{this.props.product.description}</p>
+              <p className='card-text'>{this.props.product.creator}</p>
               <AddToCartButton product={this.props.product} cart={this.props.cart} />
+            </div>
+          </div>
+
+          <div className='col-6 col-md-3'>
+            <div className='card-body'>
+              <p className='card-video'>{this.props.product.creator}</p>
             </div>
           </div>
           
         </div>
-      </div></Col>    
+      </div>
+      </Col>    
+      
+      <Col md={4}>
+        <iframe width="350" height="200" src="https://www.youtube.com/embed/OsXedJq1aWE"></iframe>
+      </Col>
+      </Row>
     );
   }
 
